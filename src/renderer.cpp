bba1f5d6b497e2b81e0e81f634af95a307dc392f
joakimthun <joakimthun@gmail.com>
2017-04-02 21:13:48 +0200

    the renderer now keeps an internal pointer to the back buffer

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -6,17 +6,19 @@ namespace kiwi {
 
 	Renderer::Renderer(Window *window)
 		:
-		window_(window)
+		window_(window),
+		back_buffer_(window->back_buffer_),
+		back_buffer_size_(window->back_buffer_size_)
 	{
 	}
 
 	void Renderer::clear(uint8_t r, uint8_t g, uint8_t b)
 	{
-		for (auto i = 0; i < window_->back_buffer_size_; i += 3)
+		for (auto i = 0; i < back_buffer_size_; i += 3)
 		{
-			window_->back_buffer_[i] = b;
-			window_->back_buffer_[i + 1] = g;
-			window_->back_buffer_[i + 2] = r;
+			back_buffer_[i] = b;
+			back_buffer_[i + 1] = g;
+			back_buffer_[i + 2] = r;
 		}
 	}
 
--- src/renderer.h
+++ src/renderer.h
@@ -14,6 +14,8 @@ namespace kiwi {
 		void clear(uint8_t r, uint8_t g, uint8_t b);
 	private:
 		Window *window_;
+		uint8_t *back_buffer_;
+		const uint64_t back_buffer_size_;
 	};
 
 }
